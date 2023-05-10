2256dbef438542837064537cc83166257b00c0b0
Alice Nocquet <al.nocquet@free.fr>
2023-05-10 15:56:58 +0200

    Phase 4 - Téléchargement images livres dossier Images_books; +Gestion doublons livres

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-import requests # permet d'envoyer des requêtes HTTP en utilisant Python. La requête HTTP renvoie un objet de réponse avec toutes les données de réponse (contenu, encodage, statut, etc.).
-from bs4 import BeautifulSoup # bibliothèque Python d'analyse syntaxique de documents HTML et XML
-import os #(operating system) - interargir avec le system d'exploitation
-import urllib.request # functions and classes which help in opening URLs
-import csv # (Comma Separated Values, valeurs séparées par des virgules) - importation et exportation bases de données
+import requests
+from bs4 import BeautifulSoup
+import os
+import urllib.request
+import csv 
 
 
-# 1 définition = 1 action
-
-
-def app():                      # Englober les def
-    extract_categories_urls()   # Def de départ
+def app():                      
+    extract_categories_urls()   
 
 
 def extract_categories_urls():
@@ -18,13 +15,16 @@ def extract_categories_urls():
                                     # Définir lien page index, 2, 3, si existent (Cibler balise html, modifier, incrémenter, condition/boléens);
                                     # Ajouter liste (Création liste) ;
                                     # Catégorie par catégorie = boucle ;
+                                    # Gérer les doublons (listes de comparaison, boucle) ;
 
-    root_url = "http://books.toscrape.com/"  # Url racine
+    liste_titres = [] # Création liste référence pour gestion des doublons
+
+    root_url = "http://books.toscrape.com/" 
     response = requests.get(root_url)           
 
     if response.ok:
         soup = BeautifulSoup(response.content, "lxml")
-        uls = soup.find("ul", {"class": "nav nav-list"}).find("ul").find_all("li") # Cibler Nav-list (ul) catégorie
+        uls = soup.find("ul", {"class": "nav nav-list"}).find("ul").find_all("li") 
 
         categories_urls = [] # Création liste urls des pages des uls (catégories)
 
@@ -33,34 +33,40 @@ def extract_categories_urls():
                                             # 2) Puis définir lien page 2 (modifier url Index) + (si existe) ajouter liste, sinon STOP ;
                                             # 3) Puis définir lien page 3 (Incrémenter page 2) + (si existe) ajouter liste, sinon STOP ;
 
-            link = ul.find("a")["href"] # Cibler balise lien chaque cellule Nav-list : < Ex.: a href="../historical-fiction_4/index.html">
-            name = link.replace("catalogue/category/books/", "").replace("/index.html", "") # Isoler noms catégories pour noms fichiers csv
-            name_cat = name.rstrip() # Supprimer \n cachés noms catégories pour noms fichiers csv
-            full_link = root_url + link # Définir (reconstruire) url : "http://books.toscrape.com/"+ <a href> = "http://books.toscrape.com/nom_catégorie_nbr/Index.html"
-            categories_urls.append(full_link) # Ajouter url index à la liste
-            url = full_link.replace("index.html", "page-") # Remplacer "index.html" par "page-"
+            link = ul.find("a")["href"] 
+            name = link.replace("catalogue/category/books/", "").replace("/index.html", "")
+            name_cat = name.rstrip() 
+            full_link = root_url + link 
+            categories_urls.append(full_link) 
+            url = full_link.replace("index.html", "page-") 
             i = 2
             response_page = requests.get(url + str(i) + ".html") # Requête page 2 = "http://books.toscrape.com/nom_catégorie_nbr/page-2.html"
-                                                                 # full_link(root_url + link) + 2 + ".html"
-            search = False # (false/true = opérateur de comparaison) Si pas de page 2, fin de boucle pour la catégorie en cours, passer à la suivante
 
+            search = False
             if response_page: 
-                search = True # Si ok
-                categories_urls.append(url + str(i) + ".html") # Ajouter url catégorie en cours de boucle "page 2" à la liste
+                search = True 
+                categories_urls.append(url + str(i) + ".html") 
 
-            while search: # "tant que mon expression logique est vraie, exécute le bloc d’instruction suivant:"
-                          # "Sinon, sort de la boucle sans exécuter les instructions". Attention, pas de boucle infinie!!! Il faut un false.
-                          # Avec if, boucle conditionnelle / On ne connait pas le nombre d'itérations
-                i += 1    # Incrémenter
+            while search: 
+                i += 1 
                 response_page = requests.get(url + str(i) + ".html") # Requête page 3 = "http://books.toscrape.com/nom_catégorie_nbr/page-3.html"
-                                                                     # full_link(root_url + link) + 3 + ".html"
-                if not response_page:
-                    search = False # Si pas de page 3, fin de boucle pour la catégorie en cours, passer à la suivante
-                else:
-                    categories_urls.append(url + str(i) + ".html") # Sinon, Ajouter url catégorie en cours de boucle "page 3" à la liste
 
-            urls_books = extract_books_urls(categories_urls) # Appeler def urls livres // Redéfinir "urls_books" pour insertion extract_books_data(urls_books, name_cat)
-            extract_books_data(urls_books, name_cat) # Appeler def données livres
+                if not response_page:
+                    search = False
+                else:
+                    categories_urls.append(url + str(i) + ".html")
+
+            urls_books = extract_books_urls(categories_urls)
+
+            new_book = [] # Création liste sans doublons pour comparaison à liste_titres
+            for book in urls_books : #### BOUCLE : Lister les livres dans une première liste pour affiner dans la seconde
+                if book not in liste_titres : # Condition : "si le livre n'existe pas dans liste_titres, ajouter dans new_book"
+                    liste_titres.append(book) # Première liste de référence
+                    new_book.append(book) # Liste affinée utilisée pour extraction data ;
+
+            extract_books_data(new_book, name_cat) # Appeler def données livres
+
+    print(len(liste_titres)) # Print liste sans doublons des titres
 
 
 def extract_books_urls(categories_urls): # Appeler liste urls pages
@@ -71,23 +77,22 @@ def extract_books_urls(categories_urls): # Appeler liste urls pages
                                                 # Ajouter liste (Création liste) ;
                                                 # Appliquer Url livre par Url livre = Boucle ;
 
-    urls_books = [] # Création liste urls livres
+    urls_books = [] # Création liste urls livres (Gestion doublons)
 
     for link in categories_urls:
         response = requests.get(link)
 
         if response.ok:
             soup = BeautifulSoup(response.content, "lxml")
-            lis = soup.findAll("li", {"class": "col-xs-6 col-sm-4 col-md-3 col-lg-3"}) # Cibler balises li + class des liens urls des livres
+            lis = soup.findAll("li", {"class": "col-xs-6 col-sm-4 col-md-3 col-lg-3"})
 
-            for li in lis:          #### BOUCLE CHANGEMENT LIEN SUR CHAQUE URL puis LISTE (urls finissent tous par index.html)
-                                    # Définir url livre (modifier) + ajouter liste, puis passer à la suivante ;
+            for li in lis:          
                 a = li.find("a")
                 link = a["href"]
                 clean = link.replace("../../../", "")                             # Exemple : <a href="../../../titre_livre/index.html">
                 urls_books.append("http://books.toscrape.com/catalogue/" + clean) # http://books.toscrape.com/catalogue/ + fin lien livre, ajouter.
 
-    return urls_books # Conserver > compiler avec def extract_categories_urls()
+    return urls_books
 
 
 def extract_books_data(urls_books, name_cat): # Appeler liste urls livres, noms fichiers csv
@@ -98,32 +103,35 @@ def extract_books_data(urls_books, name_cat): # Appeler liste urls livres, noms 
                                     #### 3) EXTRAIRES LES IMAGES DE COUVERTURE ET LES RENOMMER AVEC TITRES CORRESPONDANTS : 
                                     # Appliquer Livre par livre = Boucle ;
             
-    if not os.path.exists("./CSV_books"): # Répertoire racine où se trouve l'app : "si dossier CSV_books n'existe pas, le créer "
+    if not os.path.exists("./CSV_books"):
             os.makedirs("./CSV_books")
 
-    with open("./CSV_books/" + name_cat + ".csv", "w", encoding="utf-8-sig", newline="") as outf: # A cet endroit, ouvrir un csv avec nom catégorie correspondante
+    with open("./CSV_books/" + name_cat + ".csv", "w", encoding="utf-8-sig", newline="") as outf:
             writer = csv.writer(outf, delimiter="|")
             outf.write("URL PRODUIT" + "," + "UPC" + "," + "TITRE" + "," + "PRIX TTC" + "," + "PRIX HT" + "," + "STOCK DISPONIBLE" + "," + "DESCRIPTION" + "," 
                    + "CATEGORIE" + "," + "NOTE" + "," + "URL IMAGE" + "\n") # En têtes
         
             for link in urls_books:
-                books_data = []               # Création liste 10 datas livres
-                response = requests.get(link) # Requête lien url livre en cours
+                books_data = []               
+                response = requests.get(link)
                 if response.ok:
                     soup = BeautifulSoup(response.content, "html.parser")
                         
-                    infos_tableur = soup.find("table", {"class" : "table table-striped"}).find_all("td") # Cibler cellules tableaux DIV pour se déplacer via [valeur]
+                    infos_tableur = soup.find("table", {"class" : "table table-striped"}).find_all("td")
                     info_product_description = soup.findAll('p')
                     info_category = soup.find("ul", {"class": "breadcrumb"}).find_all("a")
 
-                    product_page_url = link  # Lien url livre en cours
-                    books_data.append(product_page_url) # Ajouter à la liste datas livres
+                    product_page_url = link
+                    books_data.append(product_page_url)
                             
-                    universal_product_code = infos_tableur[0].text # Cibler valeur [0] + format texte
+                    universal_product_code = infos_tableur[0].text
                     books_data.append(universal_product_code)
 
                     title = soup.find("li", {"class" : "active"}).text
                     books_data.append(title)
+
+                    titres = soup.find("li", {"class" : "active"}).text # Définir noms livres format texte pour images
+                    titre = titres.rstrip()                             # Enlever espaces \n cachés
 
                     price_including_tax = infos_tableur[3].text
                     books_data.append(price_including_tax)
@@ -148,6 +156,12 @@ def extract_books_data(urls_books, name_cat): # Appeler liste urls livres, noms 
                     cleaner = img_url.replace("../../", "")     # Enlever début cible html ; Ex : <img src="../../media/cache/fe/72/fe72f0532301ec28892ae79a629a293c.jpg">
                     image_url = ("http://books.toscrape.com/" + cleaner) # Définir (reconstruire) url : http://books.toscrape.com/catalogue/nomImage.jpg
                     books_data.append(image_url)
+
+                    if not os.path.exists("./Images_books"): # Répertoire racine où se trouve l'app : "si dossier Images_books n'existe pas, le créer "
+                        os.makedirs("./Images_books")
+                    with open("./Images_books/" + titre.replace(":", "").replace("/", "").replace("-", "").replace("\"", "").replace("*", "").replace("?", "").replace("_", "").replace(",", "").replace("#", "") + ".jpg", 'wb') as image:
+                        image.write(urllib.request.urlopen(image_url).read()) # A cet endroit, rappatrier image via urllib.request
+                                                                              # Définir (reconstruire) nom image via Titre + enlever caractères spéciaux
                     
                     writer.writerow(books_data) # colonnes = datas livres
 
